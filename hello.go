5946f6e2ff28f4ed1477d5db96bc8d1aa0189d2c
Luca Zoppetti <zoppe2304@gmail.com>
2021-02-06 10:54:08 +0100

    first commit

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/event_handler", func(c *gin.Context) {
+		fmt.Println("Event received")
+	})
+	
+	r.Run()
+}
